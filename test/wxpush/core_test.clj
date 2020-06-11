c3010ba5db7ee8b8dcc7de0006b5780e779b79bb
ntestoc3 <ntestoc3@outlook.com>
2020-06-11 19:33:37 +0800

    删掉测试，依赖于token

--- test/wxpush/core_test.clj
+++ test/wxpush/core_test.clj
@@ -7,11 +7,11 @@
 (deftest a-test
   (testing "gen code"
 
-    (is (= (:code (make-qrcode token))
-           1000))
+    ;; (is (= (:code (make-qrcode token))
+    ;;        1000))
 
-    (is (= (:code (send-message token {:text "aaa"
-                                       :uids "bbb"}))
-           1000))
+    ;; (is (= (:code (send-message token {:text "aaa"
+    ;;                                    :uids "bbb"}))
+    ;;        1000))
 
     ))
