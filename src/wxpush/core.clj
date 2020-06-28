3241ebe33a7965c22db03ec4042b530b4d7c47f2
ntestoc3 <ntestoc3@outlook.com>
2020-06-28 15:50:38 +0800

    修复all-uid调用错误

--- src/wxpush/core.clj
+++ src/wxpush/core.clj
@@ -27,30 +27,31 @@
 
 (defn send-message
   "发送消息给用户，不指定uids则发送给所有人"
-  [app-token {:keys [content-type uids text]
-              :or {content-type :text}}]
-  (let [text (if (> (count text) 20000)
-               (subs text 0 20000)
-               text)
-        uids (cond
-               (nil? uids) (all-uids)
+  ([app-token text] (send-message app-token text nil))
+  ([app-token text {:keys [content-type uids]
+                    :or {content-type :text}}]
+   (let [text (if (> (count text) 20000)
+                (subs text 0 20000)
+                text)
+         uids (cond
+                (nil? uids) (all-uids app-token)
 
-               (string? uids) [uids]
+                (string? uids) [uids]
 
-               :default uids)]
-    (log/info send-message text uids)
-    (let [r (-> (http/post
-                 "http://wxpusher.zjiecode.com/api/send/message"
-                 {:content-type :json
-                  :body (json/encode {:appToken app-token
-                                      :content text
-                                      :contentType (content-type-map content-type)
-                                      :uids uids})
-                  :as :json})
-                :body)]
-      (when-not (:success r)
-        (log/warn :send-message text :return r))
-      r)))
+                :default uids)]
+     (log/info send-message text uids)
+     (let [r (-> (http/post
+                  "http://wxpusher.zjiecode.com/api/send/message"
+                  {:content-type :json
+                   :body (json/encode {:appToken app-token
+                                       :content text
+                                       :contentType (content-type-map content-type)
+                                       :uids uids})
+                   :as :json})
+                 :body)]
+       (when-not (:success r)
+         (log/warn :send-message text :return r))
+       r))))
 
 (defn make-qrcode
   ([app-token] (make-qrcode app-token nil))
